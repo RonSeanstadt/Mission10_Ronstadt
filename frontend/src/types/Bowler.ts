2e00e7785d895d7840d614e55fbdc435c15da0a0
RonSeanstadt <sronstadt470@gmail.com>
2024-03-15 14:25:31 -0600

    Added frontend features for Mission 10

---
+++ frontend/src/types/Bowler.ts
@@ -0,0 +1,10 @@
+export type Bowler = {
+  bowlerId: number;
+  bowlerName: string;
+  teamName: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  phoneNumber: string;
+};
